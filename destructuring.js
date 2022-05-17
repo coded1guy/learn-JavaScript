92c1492601c646a91119ea4bc1fb9df0845bbd1d
Emmanuel Olubiyi <42422184+coded1guy@users.noreply.github.com>
2022-05-17 19:04:39 +0100

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+console.log("learning destructuring ...... ");
+
+
+const data = {
+    boy: ["fsd", "sff", "edef"]
+};
+
+
+(function boys() {
+    data.boy.forEach(element => {
+        console.log(element)
+    })
+})()
