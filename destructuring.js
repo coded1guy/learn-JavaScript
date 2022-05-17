ab3e1460ebb0cbbf84691e4b3a292e75c14db1f8
Emmanuel Olubiyi <42422184+coded1guy@users.noreply.github.com>
2022-05-17 20:32:42 +0100

    Update destructuring.js

--- destructuring.js
+++ destructuring.js
@@ -10,4 +10,19 @@ const data = {
     data.boy.forEach(element => {
         console.log(element)
     })
-})()
+})();
+
+
+let bord = [1,2,3,4];
+
+
+
+var tmp;
+var [a,b,c,d] = tmp = bord;
+
+
+console.log(a,b,c);
+
+
+bord[0] = 9;
+console.log(a,tmp);
